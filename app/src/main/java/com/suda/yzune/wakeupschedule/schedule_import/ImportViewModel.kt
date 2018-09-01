253600ee1d97a43dd838b9997a5817008da178ad
YZune <windowsyangzeng@hotmail.com>
2018-09-01 21:50:58 +0800

    小修复

--- app/src/main/java/com/suda/yzune/wakeupschedule/schedule_import/ImportViewModel.kt
+++ app/src/main/java/com/suda/yzune/wakeupschedule/schedule_import/ImportViewModel.kt
@@ -24,7 +24,7 @@ class ImportViewModel : ViewModel() {
     private val other = arrayOf("时间", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日", "早晨", "上午", "下午", "晚上")
     private val pattern1 = Pattern.compile("\\{第\\d{1,2}[-]*\\d*周")
     private val WEEK = arrayOf("", "周一", "周二", "周三", "周四", "周五", "周六", "周日")
-    private val courseProperty = arrayOf("必修", "选修", "专基", "专选", "公必", "公选", "义修")
+    private val courseProperty = arrayOf("必修", "选修", "专基", "专选", "公必", "公选", "义修", "选", "必")
     //todo: 在线更新规则
     private var selectedYear = ""
     private var selectedTerm = ""
