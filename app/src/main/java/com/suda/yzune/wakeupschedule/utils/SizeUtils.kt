e208b41896ab4e3cc895708675d47ec110a31eb0
YZune <windowsyangzeng@hotmail.com>
2018-10-22 17:33:47 +0800

    把工具类用kotlin改写

---
+++ app/src/main/java/com/suda/yzune/wakeupschedule/utils/SizeUtils.kt
@@ -0,0 +1,26 @@
+package com.suda.yzune.wakeupschedule.utils
+
+import android.content.Context
+
+object SizeUtils {
+
+    fun dp2px(context: Context, dpValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
+
+    fun px2dp(context: Context, pxValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
+    }
+
+    fun sp2px(context: Context, spValue: Float): Int {
+        val fontScale = context.resources.displayMetrics.scaledDensity
+        return (spValue * fontScale + 0.5f).toInt()
+    }
+
+    fun px2sp(context: Context, pxValue: Float): Int {
+        val fontScale = context.resources.displayMetrics.scaledDensity
+        return (pxValue / fontScale + 0.5f).toInt()
+    }
+}
---
+++ app/src/main/java/com/suda/yzune/wakeupschedule/utils/ViewUtils.kt
@@ -0,0 +1,126 @@
+package com.suda.yzune.wakeupschedule.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Point
+import android.os.Build
+import android.os.Environment
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.ScrollView
+import com.suda.yzune.wakeupschedule.R
+import java.io.File
+import java.io.FileOutputStream
+
+object ViewUtils {
+    fun fullScreen(activity: AppCompatActivity) {
+        activity.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val window = activity.window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = activity.applicationContext.getColor(R.color.transparent)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = activity.window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = activity.resources.getColor(R.color.transparent)
+        }
+    }
+
+    private fun getStatusBarHeight(context: Context): Int {
+        var result = 0
+        val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = context.resources.getDimensionPixelSize(resourceId)
+        }
+        return result
+    }
+
+    fun resizeStatusBar(context: Context, view: View) {
+        val layoutParams = view.layoutParams
+        layoutParams.height = ViewUtils.getStatusBarHeight(context.applicationContext)
+        view.layoutParams = layoutParams
+    }
+
+    fun createColorStateList(normal: Int, pressed: Int, focused: Int, unable: Int): ColorStateList {
+        val colors = intArrayOf(pressed, focused, normal, focused, unable, normal)
+        val states = arrayOfNulls<IntArray>(6)
+        states[0] = intArrayOf(android.R.attr.state_pressed, android.R.attr.state_enabled)
+        states[1] = intArrayOf(android.R.attr.state_enabled, android.R.attr.state_focused)
+        states[2] = intArrayOf(android.R.attr.state_enabled)
+        states[3] = intArrayOf(android.R.attr.state_focused)
+        states[4] = intArrayOf(android.R.attr.state_window_focused)
+        states[5] = intArrayOf()
+        return ColorStateList(states, colors)
+    }
+
+    fun getRealSize(activity: Activity): Point {
+        val size = Point()
+        activity.windowManager.defaultDisplay.getRealSize(size)
+        return size
+    }
+
+    fun saveImg(bitmap: Bitmap) {
+        //把图片存储在哪个文件夹
+        val file = File(Environment.getExternalStorageDirectory(), "DCIM")
+        if (!file.exists()) {
+            file.mkdir()
+        }
+        //图片的名称
+        val name = "mz.jpg"
+        val file1 = File(file, name)
+        if (!file1.exists()) {
+            try {
+                val fileOutputStream = FileOutputStream(file1)
+                //这个100表示压缩比,100说明不压缩,90说明压缩到原来的90%
+                //注意:这是对于占用存储空间而言,不是说占用内存的大小
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream)
+                fileOutputStream.flush()
+                fileOutputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
+        //通知图库即使更新,否则不能看到图片
+        //activity.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + file1.getAbsolutePath())));
+    }
+
+    fun getViewBitmap(scrollView: ScrollView): Bitmap {
+        var h = 0
+        val bitmap: Bitmap
+        // 获取scrollView实际高度,这里很重要
+        for (i in 0 until scrollView.childCount) {
+            h += scrollView.getChildAt(i).height
+        }
+        // 创建对应大小的bitmap
+        bitmap = Bitmap.createBitmap(scrollView.width, h,
+                Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        scrollView.draw(canvas)
+        return bitmap
+    }
+
+
+    fun layoutView(v: View, width: Int, height: Int) {
+        // validate view.width and view.height
+        v.layout(0, 0, width, height)
+        val measuredWidth = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY)
+        val measuredHeight = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY)
+
+        // validate view.measurewidth and view.measureheight
+        v.measure(measuredWidth, measuredHeight)
+        v.layout(0, 0, v.measuredWidth, v.measuredHeight)
+    }
+
+
+}
