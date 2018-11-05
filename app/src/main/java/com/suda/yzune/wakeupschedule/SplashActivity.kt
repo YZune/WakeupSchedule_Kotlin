fefddc0d58b1ae9ecc0fb522af64c84eb0c3beef
YZune <windowsyangzeng@hotmail.com>
2018-11-05 22:44:20 +0800

    集成fabric后台统计

--- app/src/main/java/com/suda/yzune/wakeupschedule/SplashActivity.kt
+++ app/src/main/java/com/suda/yzune/wakeupschedule/SplashActivity.kt
@@ -3,15 +3,19 @@ package com.suda.yzune.wakeupschedule
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
 import com.suda.yzune.wakeupschedule.schedule.ScheduleActivity
 import com.suda.yzune.wakeupschedule.utils.UpdateUtils
 import es.dmoral.toasty.Toasty
+import io.fabric.sdk.android.Fabric
 import org.jetbrains.anko.startActivity
+
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         UpdateUtils.tranOldData(applicationContext)
         Toasty.Config.getInstance()
                 .setToastTypeface(Typeface.DEFAULT_BOLD)
