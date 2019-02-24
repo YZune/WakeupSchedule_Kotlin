d41fad938e79128fd81bcc492d4395bf239ac8a0
YZune <windowsyangzeng@hotmail.com>
2019-02-24 22:30:13 +0800

    修复一处 bug

--- app/src/main/java/com/suda/yzune/wakeupschedule/schedule_import/WebViewLoginFragment.kt
+++ app/src/main/java/com/suda/yzune/wakeupschedule/schedule_import/WebViewLoginFragment.kt
@@ -162,7 +162,7 @@ class WebViewLoginFragment : BaseFragment() {
         }
 
         var zfChipId = 0
-        cg_qz.setOnCheckedChangeListener { chipGroup, id ->
+        cg_zf.setOnCheckedChangeListener { chipGroup, id ->
             when (id) {
                 R.id.chip_zf1 -> {
                     zfChipId = id
